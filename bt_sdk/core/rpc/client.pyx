fdd7b32b81c6ccaa40888f47e06f81eb377710f5
hengxin.liu <hengxin.liu@inceptio.ai>
2026-07-11 18:56:01 +0800

    revise bug in rpc client

--- bt_sdk/core/rpc/client.pyx
+++ bt_sdk/core/rpc/client.pyx
@@ -29,6 +29,9 @@ cdef object Scale = {
         RpcTopic.Close: {
             "tick": 1.0, "open": 1e-5, "high": 1e-5, "low": 1e-5, "close": 1e-5, "volume": 1e-3, "amount": 1e-3,
         },
+        RpcTopic.Daily: {
+            "tick": 1.0, "open": 1e-5, "high": 1e-5, "low": 1e-5, "close": 1e-5, "volume": 1e-3, "amount": 1e-3,
+        },
         RpcTopic.Adjustment: {
             "bonus_share": 1e-3, "transfer": 1e-3, "bonus": 1e-3, # adjustment
         },
